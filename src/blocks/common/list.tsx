ab3d8e56bf69ee2f8b6919669c6d9f0a53bec92e
jaychirgwin <jason@jaycmedia.com>
2023-02-01 00:12:25 +1100

    Implement column width and list innerBlocks

--- src/blocks/common/list.tsx
+++ src/blocks/common/list.tsx
@@ -1,14 +1,24 @@
-import { IWPGBlock } from '../../types'
+import {IWPGBlock} from '../../types'
 import * as React from 'react'
 
 const WPGListBlock: React.FC<IWPGBlock> = (props) => {
   const {
     // attrs,
-    // innerBlocks,
+    innerBlocks,
     innerHTML
   } = props
 
-  return <div className="wpg-block wpg-b_list" dangerouslySetInnerHTML={{ __html: innerHTML }} />
+  const items = innerBlocks.map((item, i) => {
+    return (
+      <li className={`wp-block-list-item ${i + 1}-list-item`} key={i}
+          dangerouslySetInnerHTML={{__html: item.innerHTML}}
+      />
+    )
+  })
+
+  return <ul className="wpg-block wpg-b_list">{items}</ul>
+
+  // return <div className="wpg-block wpg-b_list" dangerouslySetInnerHTML={{__html: innerHTML}}/>
 }
 
 export default WPGListBlock
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react'
-import { GetTheBlock } from './blocks'
-import { IWPGBlocksProps, IWPGBlockProps } from './types'
+import {GetTheBlock} from './blocks'
+import {IWPGBlockProps, IWPGBlocksProps} from './types'
 
-const WPGBlocks: React.FC<IWPGBlocksProps> = ({ blocks, mapToBlock }) => {
+const WPGBlocks: React.FC<IWPGBlocksProps> = ({blocks, mapToBlock}) => {
   return (
     <div className="wpg-blocks">
       {blocks
         .filter((block) => !!block.blockName)
         .map((block, index) => (
-          <WPGBlock key={index} block={block} mapToBlock={mapToBlock} />
+          <WPGBlock key={index} block={block} mapToBlock={mapToBlock}/>
         ))}
     </div>
   )
 }
 
-export const WPGBlock: React.FC<IWPGBlockProps> = ({ block, mapToBlock }) => {
-  const { blockName, attrs, innerBlocks, innerHTML, innerContent } = block
+export const WPGBlock: React.FC<IWPGBlockProps> = ({block, mapToBlock}) => {
+  const {blockName, attrs, innerBlocks, innerHTML, innerContent} = block
 
   if (!blockName) return null
 
